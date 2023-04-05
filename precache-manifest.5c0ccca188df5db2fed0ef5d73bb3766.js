c16063f781652268fa44e5bb2122b90c88212418
kaihoefler <26207541+kaihoefler@users.noreply.github.com>
2023-04-05 15:32:22 +0000

    Deploying to gh-pages from @ kaihoefler/livestream@8883cefe2a7bf0dc5f60de87b480c83b0bac3a5d 🚀

---
+++ precache-manifest.5c0ccca188df5db2fed0ef5d73bb3766.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc2bb2ce7ad0ee90b29f51e6a7032df",
+    "url": "/livestream/Race_times_1h_Finish.json"
+  },
+  {
+    "revision": "9b84ef9835bf96a1a78bd28de6233d1a",
+    "url": "/livestream/Race_times_1h_race.json"
+  },
+  {
+    "revision": "3dd01816df214a0f8f9fbd6d0d950093",
+    "url": "/livestream/Race_times_500m_End.json"
+  },
+  {
+    "revision": "66982c31b6d21f329102506bbf8a910a",
+    "url": "/livestream/Race_times_500m_End1.json"
+  },
+  {
+    "revision": "3f44538e6b1a3bbb8c22102359b4a7e2",
+    "url": "/livestream/Race_times_500m_Startlist.json"
+  },
+  {
+    "revision": "6e1385d772a5b386196d5ed61b91e9a5",
+    "url": "/livestream/Race_times_Dobbin_Finish.json"
+  },
+  {
+    "revision": "27b9ce5cfeb8926c0d8aa4cb068146e0",
+    "url": "/livestream/Race_times_Dobbin_Startlist.json"
+  },
+  {
+    "revision": "41cc6f25ff10123fdc53bf0630ab302b",
+    "url": "/livestream/Race_times_Dobbin_race_1.json"
+  },
+  {
+    "revision": "a18a021ebd97f8eda3f7253fc0470474",
+    "url": "/livestream/Race_times_Dobbin_race_2.json"
+  },
+  {
+    "revision": "91766b2c3ecf80bf864a916c1a068ef7",
+    "url": "/livestream/Race_times_Dobbin_race_3.json"
+  },
+  {
+    "revision": "17227b9a20ebc9075a47",
+    "url": "/livestream/css/app.897906df.css"
+  },
+  {
+    "revision": "fe5ad05a51acf9b20cac257a224d7f8c",
+    "url": "/livestream/css/font-awesome.min.css"
+  },
+  {
+    "revision": "3db8304d12f988c890b392f9fe753d8b",
+    "url": "/livestream/css/traina_bulma.css"
+  },
+  {
+    "revision": "1e0a6b885de6cdad40e0f5695cf3dbd1",
+    "url": "/livestream/fonts/ubuntu.1e0a6b88.woff"
+  },
+  {
+    "revision": "79feb15aec791cd009a73a0a286ed3af",
+    "url": "/livestream/index.html"
+  },
+  {
+    "revision": "17227b9a20ebc9075a47",
+    "url": "/livestream/js/app.b093362f.js"
+  },
+  {
+    "revision": "0280a9798a01fc591d40",
+    "url": "/livestream/js/chunk-vendors.5e4ad812.js"
+  },
+  {
+    "revision": "0bf13a5e68b338d996a924c1199a3665",
+    "url": "/livestream/manifest.json"
+  },
+  {
+    "revision": "d194a4700a08b142aeafd12a7ccc42ee",
+    "url": "/livestream/race_status._Elimination_with_finish.json"
+  },
+  {
+    "revision": "ef5b3d665863ce362ce41f08f239233e",
+    "url": "/livestream/race_status._Elimination_without_finish.json"
+  },
+  {
+    "revision": "a0540d33cff1f8585fd4eb7804a9536b",
+    "url": "/livestream/race_status_Points_with_finish.json"
+  },
+  {
+    "revision": "2f6ed2a9f608661771e77dbf592be091",
+    "url": "/livestream/race_status_Points_without_finish.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/livestream/robots.txt"
+  }
+]);
